b11c1af04f65f7c7ee4500daf12d9290cacb981b
ittat <ittat@live.com>
2020-08-10 10:59:08 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -368,9 +368,6 @@ PRODUCT_COPY_FILES += \
 # UBPorts
 PRODUCT_PACKAGES += \
      libubuntu_application_api \
-     direct_ubuntu_application_sensors_c_api_for_hybris_test \
-     direct_ubuntu_application_sensors_for_hybris_test \
-     direct_ubuntu_application_gps_c_api_for_hybris_test \
      libcamera_compat_layer \
      libmedia_compat_layer_32 \
      libaudioflingerglue \
